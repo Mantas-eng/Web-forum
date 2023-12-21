eaec5ec3e097294528c2d274ffa219938c657af4
Mantas <mantaskreivys7@gmail.com>
2023-12-21 05:31:35 +0200

    adding new features to Forum_list

--- src/Components/Forum-List/Forum-List.tsx
+++ src/Components/Forum-List/Forum-List.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Image from 'next/image';
+import styles from '../Forum-List/styles.module.css';
+import Link from "next/link";
 
 const ForumList = () => {
   return (
